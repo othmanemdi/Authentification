ef3d19693cd2d413bce1611ab4f7f5ee7f9f7e32
othmanemdi <mo.atlantique@gmail.com>
2023-09-19 03:10:26 +0100

    Fix old commit

--- register.php
+++ register.php
@@ -1,93 +1,18 @@
 <?php
 require_once "database/db.php";
 require_once "helpers/functions.php";
-$page = _get_page_name();
+$page = "register";
 $title = "register";
 
-$errors = [];
 
 if (isset($_POST['register'])) {
 
-    if (isset($_POST['nom'], $_POST['email'], $_POST['password'], $_POST['confirm_password'])) {
+    $nom = e($_POST['nom']);
+    $email = e($_POST['email']);
+    $password = e($_POST['password']);
 
-        $nom = e($_POST['nom']);
-        $email = e($_POST['email']);
-        $password = e($_POST['password']);
-        $confirm_password = e($_POST['confirm_password']);
-
-        if ($nom == '') {
-            $errors[] = "Veuillez saisire le champ (nom)";
-        }
-
-        if (strlen($nom) < 3) {
-            $errors[] = "(nom) Veuillez saisire Plus que 3 charactéres";
-        }
-
-        if (!preg_match('/^[a-zA-Z]+$/', $nom)) {
-            $errors[] = "(nom) Seul les charactére sont autoriser";
-        }
-
-
-        if ($email == '') {
-            $errors[] = "Veuillez saisire le champ (email)";
-        }
-
-        if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
-            $errors[] = "Email invalide";
-        }
-
-        // $email_rows = $db->query("SELECT id FROM users WHERE email = '$email' LIMIT 1")->rowCount();
-
-        $req = $db->prepare("SELECT id FROM users WHERE email = ? LIMIT 1");
-        $req->execute([$email]);
-        $email_rows =  $req->rowCount();
-
-        if ($email_rows == 1) {
-            $errors[] = "Cet email '" . $email . "' est déja pris";
-        }
-
-        if ($password == '') {
-            $errors[] = "Veuillez saisire le champ (password)";
-        }
-
-        if (strlen($password) < 6) {
-            $errors[] = "(password) Veuillez saisire Plus que 6 charactéres";
-        }
-
-        if ($confirm_password == '') {
-            $errors[] = "Veuillez saisire le champ (confirm_password)";
-        }
-
-        if ($password != $confirm_password) {
-            $errors[] = "Les deux mot de passe ne sont pas identique";
-        }
-    } else {
-        $errors[] = "Veuillez remplire tous les champs";
-    }
-
-    // Ok
-    if (empty($errors)) {
-
-        $password_hash = password_hash($password, PASSWORD_BCRYPT);
-
-        dd($password_hash);
-
-
-        $req = $db->prepare("INSERT INTO users SET nom = ?, email = ?, password = ? ");
-        $req->execute([$nom, $email, $password_hash]);
-        $_SESSION['message'] = 'Bien enregistrer';
-        $_SESSION['color'] = 'info';
-        header('Location: login.php');
-        exit;
-    } else {
-        dd($errors);
-    }
-
-    exit;
-
-
-
-
+    $req = $db->prepare("INSERT INTO users SET nom = ?, password = ?, email = ? ");
+    $req->execute([$_POST['nom'], $password, $_POST['email']]);
     // $user_id = $db->lastInsertId();
 
     // $req = $db->prepare("INSERT INTO user_historique SET user_id = ?, ip = ?");
@@ -119,9 +44,9 @@ if (isset($_POST['register'])) {
 
         <div class="row justify-content-center g-2 mt-5">
             <div class="col-md-4">
-                <div class="card shadow">
+                <div class="card">
                     <div class="card-header">
-                        <h5>Register</h5>
+                        <h4>Register</h4>
                     </div>
 
                     <div class="card-body">
@@ -139,11 +64,6 @@ if (isset($_POST['register'])) {
                             <div class="mb-3">
                                 <label for="password" class="form-label">Password</label>
                                 <input type="password" class="form-control" name="password" id="password" placeholder="Password:">
-                            </div>
-
-                            <div class="mb-3">
-                                <label for="confirm_password" class="form-label">Confirm Your Password</label>
-                                <input type="password" class="form-control" name="confirm_password" id="confirm_password" placeholder="Confirm Your Password:">
                             </div>
 
                             <button type="submit" name="register" class="btn btn-primary">
